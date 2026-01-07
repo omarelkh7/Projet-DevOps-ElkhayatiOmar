0e8b34e7f7ad9de07befea282c51fb4afd0cdfef
Omar El Khayati <omar7elkhayati@gmail.com>
2026-01-07 17:38:16 +0100

    Passage à un workflow Java pur sans Maven

--- devops-app/src/main/java/com/devops/projet/App.java
+++ devops-app/src/main/java/com/devops/projet/App.java
@@ -2,6 +2,6 @@ package com.devops.projet;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour et bon courage dans votre projet en DevOps!!!");
+        System.out.println("Bonj et bon courage dans votre projet en DevOps!!!");
     }
 }
